960c4baa2499f1a3324d14fc4f0b8a39f97eae0e
Dan Harcombe <dan.h@hotmail.co.uk>
2020-02-20 19:37:52 +0000

    Tidy up the starting package

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@
 """
     Setup file for collaborator.
     Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 3.2.3.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
-"""
+""
 import sys
 
 from pkg_resources import VersionConflict, require
@@ -20,4 +16,4 @@ except VersionConflict:
 
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    setup()
